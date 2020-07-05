a2aa71e6c13d7fef3e7e2617329b7f2e81c68157
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-05 19:49:55 +0300

    building user ban

--- resources/views/partials/activate-modal.blade.php
+++ resources/views/partials/activate-modal.blade.php
@@ -1,4 +1,4 @@
-<div x-data="{ open: true }" x-show="open" class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
+<div x-data="{ open: true, progress: false }" x-show="open" class="fixed bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
     <div class="fixed inset-0 transition-opacity">
         <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
     </div>
@@ -25,8 +25,8 @@
         </div>
         <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
       <span class="flex w-full rounded-md shadow-sm sm:ml-3 sm:w-auto">
-        <button @click="setTheme()" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
-          Configure
+        <button @click="progress = true; setTheme();" type="button" class="inline-flex justify-center w-full rounded-md border border-transparent px-4 py-2 bg-green-600 text-base leading-6 font-medium text-white shadow-sm hover:bg-green-500 focus:outline-none focus:border-green-700 focus:shadow-outline-green transition ease-in-out duration-150 sm:text-sm sm:leading-5">
+          <span x-show="progress" class="pr-2">@include('partials.spinner')</span> Configure
         </button>
       </span>
             <span class="mt-3 flex w-full rounded-md shadow-sm sm:mt-0 sm:w-auto">
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth.shopify'])->name('home');
+Route::group(['middleware' => 'auth.shopify'], function () {
+    Route::get('/', function () {
+        return view('dashboard');
+    })->name('home');
 
-
-Route::view('/products', 'products');
-Route::view('/customers', 'customers');
-Route::view('/settings', 'settings');
+    Route::view('/products', 'products');
+    Route::view('/customers', 'customers');
+    Route::view('/settings', 'settings');
+});
