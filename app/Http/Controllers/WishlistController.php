fb72bc151d17ee75b455b864c8be249abeea0bbc
ElijahCoding <cuiyeqing960904@gmail.com>
2020-07-07 11:18:46 +0300

    wishlist

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -7,79 +7,20 @@ use Illuminate\Http\Request;
 
 class WishlistController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        Wishlist::updateOrCreate($request->all());
+
+        return response()->json([
+            'message' => 'success'
+        ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Wishlist  $wishlist
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Wishlist $wishlist)
+    public function destroy(Request $request)
     {
-        //
-    }
+        $item = Wishlist::where("shop_id", $request['shop_id'])->where('customer_id', $request['customer_id'])
+            ->where('product_id', $request['product_id'])->first();
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Wishlist  $wishlist
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Wishlist $wishlist)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Wishlist  $wishlist
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Wishlist $wishlist)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Wishlist  $wishlist
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Wishlist $wishlist)
-    {
-        //
+        return Wishlist::destroy($item->id);
     }
 }
--- app/Wishlist.php
+++ app/Wishlist.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wishlist extends Model
 {
-    //
+    protected $fillable = [
+        'shop_id', 'customer_id', 'product_id'
+    ];
 }
